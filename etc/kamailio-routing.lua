0053cf06768e279ce447302d10871a98ea7c1dda
Daniel-Constantin Mierla <miconda@gmail.com>
2018-09-11 12:09:54 +0200

    etc/kamailio-routing.lua: added option to set project id per ip address
    - GH #14

--- etc/kamailio-routing.lua
+++ etc/kamailio-routing.lua
@@ -45,6 +45,10 @@ ALLOWADDR={
 	"147.75.60.160/28"
 };
 
+-- list of ip addresses that have the project id mapped statically
+PROJECTIPID = {}
+PROJECTIPID["127.0.0.1"] = "signalwire.localhost"
+
 -- match source ip against ALLOWADDR list
 function ksr_is_src_trusted()
 	local srcaddr = KSR.pv.get("$si");
@@ -286,29 +290,38 @@ function ksr_route_auth()
 
 	local hbody = "";
 	if uapasswd == nil or string.len(uapasswd) < 8 then
-		if KSR.hdr.is_present("Contact") > 0
-				and KSR.textops.search_hf("Contact", "x.signalwire.project", "f") > 0 then
-			local xsp = KSR.pv.gete("$(ct{tobody.params}{param.value,x.signalwire.project})");
-			if string.len(xsp) < 4 then
-				hbody = "{ \"username\": \"" .. KSR.pv.get("$fu")
-						.. "\", \"domain\": \"" .. KSR.pv.get("$fd") .. "\"}";
+		local srcaddr = KSR.pv.get("$si");
+		local xsp = "";
+		if PROJECTIPID[srcaddr] != nil then
+			if KSR.is_REGISTER() then
+				xsp = PROJECTIPID[srcaddr];
 			else
-				if string.sub(xsp, 1, 1) == "\"" and string.sub(xsp, -1, -1) == "\"" then
-					-- value is already quoted
-					hbody = "{ \"username\": \"" .. KSR.pv.get("$fu")
-						.. "\", \"domain\": \"" .. KSR.pv.get("$fd")
-						.. "\", \"project\": " .. xsp
-						.. "}";
-				else
-					hbody = "{ \"username\": \"" .. KSR.pv.get("$fu")
-						.. "\", \"domain\": \"" .. KSR.pv.get("$fd")
-						.. "\", \"project\": \"" .. xsp
-						.. "\"}";
-				end
+				KSR.sl.sl_send_reply(403, "Method restricted");
+				KSR.x.exit();
 			end
-		else
+		end
+		if string.len(xsp) < 4 then
+			if KSR.hdr.is_present("Contact") > 0
+					and KSR.textops.search_hf("Contact", "x.signalwire.project", "f") > 0 then
+				xsp = KSR.pv.gete("$(ct{tobody.params}{param.value,x.signalwire.project})");
+			end
+		end
+		if string.len(xsp) < 4 then
 			hbody = "{ \"username\": \"" .. KSR.pv.get("$fu")
 					.. "\", \"domain\": \"" .. KSR.pv.get("$fd") .. "\"}";
+		else
+			if string.sub(xsp, 1, 1) == "\"" and string.sub(xsp, -1, -1) == "\"" then
+				-- value is already quoted
+				hbody = "{ \"username\": \"" .. KSR.pv.get("$fu")
+					.. "\", \"domain\": \"" .. KSR.pv.get("$fd")
+					.. "\", \"project\": " .. xsp
+					.. "}";
+			else
+				hbody = "{ \"username\": \"" .. KSR.pv.get("$fu")
+					.. "\", \"domain\": \"" .. KSR.pv.get("$fd")
+					.. "\", \"project\": \"" .. xsp
+					.. "\"}";
+			end
 		end
 		KSR.pv.sets("$var(hres)", "");
 		KSR.http_client.query_post_hdrs(AUTHURL, hbody,
