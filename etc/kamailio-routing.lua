e0f52677a0090b72f9dcacdb69060cac4d586d75
Daniel-Constantin Mierla <miconda@gmail.com>
2018-09-18 08:59:54 +0200

    etc/kamailio-routing.lua: use ds_is_from_list_mode() instead of ds_is_from_list()

--- etc/kamailio-routing.lua
+++ etc/kamailio-routing.lua
@@ -115,7 +115,7 @@ function ksr_request_route()
 	end
 
 	-- routing inbound and outbound
-	if KSR.dispatcher.ds_is_from_list(100, 3) > 0 then
+	if KSR.dispatcher.ds_is_from_list_mode(100, 3) > 0 then
 		ksr_route_location();
 		if KSR.is_myself_ruri() then
 			KSR.sl.send_reply(404, "Local route");
@@ -250,7 +250,7 @@ end
 -- IP authorization and user uthentication
 function ksr_route_auth()
 	-- skip auth for traffic from media servers
-	if KSR.dispatcher.ds_is_from_list(100, 3) > 0 then
+	if KSR.dispatcher.ds_is_from_list_mode(100, 3) > 0 then
 		return 1;
 	end
 
