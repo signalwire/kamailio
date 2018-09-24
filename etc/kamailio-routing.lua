50c1adb952ee93e76abd94da6c6c8b496379a466
Daniel-Constantin Mierla <miconda@gmail.com>
2018-09-24 16:24:00 +0200

    etc: load db_redis, ndb_redis and permissions module
    - check if traffic comes from freeswitch via local function as well as
    permission allow source address, along with the old dispatcher list
    check
    - permissions is using db_redis backend

--- etc/kamailio-routing.lua
+++ etc/kamailio-routing.lua
@@ -51,6 +51,11 @@ ALLOWADDR={
 	"147.75.60.160/28"
 };
 
+-- list of freeswitch addresses to allow traffic from without user auth
+-- must have subnet mask (CIDR notation - use /32 for single ip addr)
+FSADDR={
+};
+
 -- list of ip addresses that have the project id mapped statically
 PROJECTIPID = {}
 PROJECTIPID["127.0.0.1"] = "signalwire.localhost"
@@ -61,6 +66,17 @@ local g_crt_projectid = ""
 function ksr_is_src_trusted()
 	local srcaddr = KSR.pv.get("$si");
 	for idx, val in pairs(ALLOWADDR) do
+		if KSR.ipops.ip_is_in_subnet(srcaddr, val) > 0 then
+			return true;
+		end
+	end
+	return false;
+end
+
+-- match source ip against FSADDR list
+function ksr_is_src_fsaddr()
+	local srcaddr = KSR.pv.get("$si");
+	for idx, val in pairs(FSADDR) do
 		if KSR.ipops.ip_is_in_subnet(srcaddr, val) > 0 then
 			return true;
 		end
@@ -133,7 +149,9 @@ function ksr_request_route()
 	end
 
 	-- routing inbound and outbound
-	if KSR.dispatcher.ds_is_from_list_mode(100, 3) > 0 then
+	if ksr_is_src_fsaddr()
+			or KSR.dispatcher.ds_is_from_list_mode(100, 3) > 0
+			or KSR.permissions.allow_source_address(100) > 0 then
 		ksr_route_location();
 		if KSR.is_myself_ruri() then
 			KSR.sl.send_reply(404, "Local route");
@@ -278,7 +296,9 @@ function ksr_route_auth()
 	g_crt_projectid = "";
 
 	-- skip auth for traffic from media servers
-	if KSR.dispatcher.ds_is_from_list_mode(100, 3) > 0 then
+	if ksr_is_src_fsaddr()
+			or KSR.dispatcher.ds_is_from_list_mode(100, 3) > 0
+			or KSR.permissions.allow_source_address(100) > 0 then
 		return 1;
 	elseif KSR.dispatcher.ds_is_from_list_mode(200, 3) > 0 then
 		return 1;
