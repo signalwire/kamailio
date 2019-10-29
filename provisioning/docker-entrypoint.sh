d153f7704c5021f9e3a350ff89728bab8854a0e5
Evan McGee <evan@signalwire.com>
2019-10-29 16:30:55 -0700

    Increase shared memory to 1024mb. Helping to address the memory fragmentation issue: https://github.com/signalwire/kamailio/issues/123

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -32,7 +32,7 @@ fi
 
 prep_term
 echo 65535 > /writeable-proc/sys/net/core/somaxconn
-/usr/local/sbin/kamailio -DD -dd -E -e -m 256 -M 12 \
+/usr/local/sbin/kamailio -DD -dd -E -e -m 1024 -M 12 \
   -A KAM_IP_LOCAL=$(ip route get 1.1.1.1 | awk 'NR==1 {print $NF}') \
   -A KAM_IP_PUBLIC=${KAM_IP_PUBLIC} \
   -A KAM_IP_OTHER=$(ip addr | grep -Po '.+10.92.+\/16.+' | grep -Po 'inet \K[\d.]+') \
