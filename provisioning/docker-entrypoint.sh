5246cd297ac877609667364292f8121e35a1cfa6
Evan McGee <evan@signalwire.com>
2021-06-08 20:31:06 -0700

    Use prep & wait_term to proxy SIGINT to Kamailio
    Without these, the signals are getting soaked by the bash prompt and the entire container is being SIGTERM'd before the profile can dump properly

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+prep_term()
+{
+    unset term_child_pid
+    unset term_kill_needed
+    trap 'handle_term' TERM INT
+}
+
+handle_term()
+{
+    if [ "${term_child_pid}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null
+    else
+        term_kill_needed="yes"
+    fi
+}
+
+wait_term()
+{
+    term_child_pid=$!
+    if [ "${term_kill_needed}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null 
+    fi
+    wait ${term_child_pid}
+    trap - TERM INT
+    wait ${term_child_pid}
+}
 
 if [ "x${KAM_IP_PUBLIC}" == "x" ]; then
   KAM_IP_PUBLIC=$(dig +short myip.opendns.com @resolver1.opendns.com)
@@ -15,12 +41,12 @@ if [[ ! -v CONFD_DISABLED ]]; then
 fi
 
 echo 65535 > /writeable-proc/sys/net/core/somaxconn
-export LD_PRELOAD=/usr/local/lib/libtcmalloc.so
-export HEAPPROFILE=/tmp/heapprof
-/usr/local/sbin/kamailio -DD -dd -E -m 2048 -M 24 \
-  -A KAM_IP_LOCAL=$(ip route get 1.1.1.1 | awk 'NR==1 {print $NF}') \
-  -A KAM_IP_PUBLIC=${KAM_IP_PUBLIC} \
-  -A KAM_IP_OTHER=$(ip addr | grep -Po '.+10.92.+\/16.+' | grep -Po 'inet \K[\d.]+') \
-  -A KAM_CLUSTER_NONCE=\"$KAM_CLUSTER_NONCE\" \
-  -A $KAMAILIO_LOCATION \
-  --log-engine=json:acA
+prep_term
+  /usr/local/sbin/kamailio -DD -dd -E -m 2048 -M 24 \
+    -A KAM_IP_LOCAL=$(ip route get 1.1.1.1 | awk 'NR==1 {print $NF}') \
+    -A KAM_IP_PUBLIC=${KAM_IP_PUBLIC} \
+    -A KAM_IP_OTHER=$(ip addr | grep -Po '.+10.92.+\/16.+' | grep -Po 'inet \K[\d.]+') \
+    -A KAM_CLUSTER_NONCE=\"$KAM_CLUSTER_NONCE\" \
+    -A $KAMAILIO_LOCATION \
+    --log-engine=json:acA &
+wait_term
