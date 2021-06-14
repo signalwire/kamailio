19a35fa1c1a9d6cea5ebd971db8665e075068a54
Evan McGee <evan@signalwire.com>
2021-06-14 01:43:41 -0700

    Set the KAM_LOCAL_IP in a more generic way
    The current `awk` line to grab the default interface requires the `src` element to be the final string element. However, in some Debian 10 instances we see `ip route get 1.1.1.1` is ending with `uid 0`
    
    So it has become `1.1.1.1 via 172.18.0.1 dev eth2 src 172.18.0.3 uid 0` vs the previously expected `1.1.1.1 via 172.18.0.1 dev eth2 src 172.18.0.3`
    
    This change just `sed`s out the middle and grabs the `src` directly, no matter where it appears.

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -48,7 +48,7 @@ fi
 echo 65535 > /writeable-proc/sys/net/core/somaxconn
 prep_term
   /usr/local/sbin/kamailio -DD -dd -E -m 2048 -M 24 \
-    -A KAM_IP_LOCAL=$(ip route get 1.1.1.1 | awk 'NR==1 {print $NF}') \
+    -A KAM_IP_LOCAL=$(ip route get 1.1.1.1 | sed 's/^.*src \([^ ]*\).*$/\1/;q') \
     -A KAM_IP_PUBLIC=${KAM_IP_PUBLIC} \
     -A KAM_IP_OTHER=$(ip addr | grep -Po '.+10.92.+\/16.+' | grep -Po 'inet \K[\d.]+') \
     -A KAM_CLUSTER_NONCE=\"$KAM_CLUSTER_NONCE\" \
