5b4de6c814e493efa1246a021b4f40214df8b69f
Chris Clement <chris.clement@signalwire.com>
2024-06-07 14:37:47 +0000

    Correct newly added IP addresses

--- etc/kamailio-routing.lua
+++ etc/kamailio-routing.lua
@@ -90,9 +90,9 @@ REJECT_600_IPS={
 -- must have subnet mask (CIDR notation - use /32 for single ip addr)
 ALLOWADDR={
     -- DT Signaling
-    "194.24.206.42/32",
+    "194.25.206.42/32",
     "194.25.206.44/32",
-    "194.24.206.170/32",
+    "194.25.206.170/32",
     "194.25.206.172/32",
     -- DT Leipzig
     "194.25.206.99/32",
